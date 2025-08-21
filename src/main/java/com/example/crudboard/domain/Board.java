f3c15b730ca0fc79e471e361393398d9e47a0f70
kim0hyeon <dbfwjs991105@gmail.com>
2025-08-21 20:41:24 +0900

    feat: Implement BoardService

--- src/main/java/com/example/crudboard/domain/Board.java
+++ src/main/java/com/example/crudboard/domain/Board.java
@@ -17,4 +17,15 @@ public class Board {
 
     @Column(name = "board_name", nullable = false, unique = true, length=30)
     private String boardName;
+
+    // 새로운 게시판을 생성할 때 사용될 생성자
+    public Board(String boardName){
+        this.boardName = boardName;
+    }
+
+    // 게시판 이름을 수정할 때 사용될 메서드
+    public void updateBoardName(String newBoardName) {
+        // 검증은 Service 클래스에서 이루어진다.
+        this.boardName = newBoardName;
+    }
 }
